20fac92287eeda80c61fe81ae16ca039a1bde2ab
Julian Lires <jlires@uc.cl>
2020-06-28 04:09:18 -0400

    Updates

---
+++ precache-manifest.03cd637535d8746f39cb4633d07746bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3de91e20593066987294040aa4d4de",
+    "url": "/IIC3103-Tarea-5/index.html"
+  },
+  {
+    "revision": "da3ac1b44877192e605b",
+    "url": "/IIC3103-Tarea-5/static/css/main.4152bfbb.chunk.css"
+  },
+  {
+    "revision": "9366b0bd95718c591e16",
+    "url": "/IIC3103-Tarea-5/static/js/2.500e7c04.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/IIC3103-Tarea-5/static/js/2.500e7c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3ac1b44877192e605b",
+    "url": "/IIC3103-Tarea-5/static/js/main.7b828d33.chunk.js"
+  },
+  {
+    "revision": "da41faec7519cf71bb7c",
+    "url": "/IIC3103-Tarea-5/static/js/runtime-main.bf4ba7ae.js"
+  },
+  {
+    "revision": "6187b0967c147230e9a3cd39e969bdbe",
+    "url": "/IIC3103-Tarea-5/static/media/get_schwifty__a_rick_and_morty_font_by_jonizaak_dajw4u8-webfont.6187b096.woff2"
+  },
+  {
+    "revision": "b95a0893690c5d2758829fd47d222fff",
+    "url": "/IIC3103-Tarea-5/static/media/get_schwifty__a_rick_and_morty_font_by_jonizaak_dajw4u8-webfont.b95a0893.woff"
+  },
+  {
+    "revision": "f5fa4799f10083e93c1a576d6537663f",
+    "url": "/IIC3103-Tarea-5/static/media/morty.f5fa4799.png"
+  },
+  {
+    "revision": "82926b7d1281f7e749da4a12ca268aa4",
+    "url": "/IIC3103-Tarea-5/static/media/rick.82926b7d.png"
+  }
+]);
